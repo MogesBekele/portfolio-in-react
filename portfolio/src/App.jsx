866e3088b842e5ce30beacf8eb43d8f0d893c8b0
MogesBekele <mogesbekele32@gmail.com>
2025-05-07 16:40:41 +0300

    tailwind css

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='text-5xl text-center items-center justify-center min-h-screen'>App</div>
-  )
-}
+    <div className="text-5xl text-center items-center justify-center ">
+      <h1 className="text-5xl text-center items-center justify-center ">
+        Hello World!
+      </h1>
+      <h2 className="text-3xl text-center items-center justify-center ">
+        This is my portfolio
+      </h2>
+    </div>
+  );
+};
 
-export default App
+export default App;
