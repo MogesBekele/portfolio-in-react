5fdf884bdd30a1322c4b5216d9729568cc442550
MogesBekele <mogesbekele32@gmail.com>
2025-05-07 16:42:43 +0300

    tailwind applied

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,11 +6,10 @@ const App = () => {
       <h1 className="text-5xl text-center items-center justify-center ">
         Hello World!
       </h1>
-      <h2 className="text-3xl text-center items-center justify-center ">
-        This is my portfolio
-      </h2>
+      
     </div>
   );
+  
 };
 
 export default App;
