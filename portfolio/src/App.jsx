6a1f23808dadef8390f6418905e6688720b51f2d
MogesBekele <mogesbekele32@gmail.com>
2025-05-07 16:48:33 +0300

    tailwind css appied

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,31 +3,56 @@ import React from "react";
 const App = () => {
   return (
     <div className="text-5xl text-center items-center justify-center ">
-
-
-    
-
-      <h1 className="text-5xl text-center items-center justify-center">My Portfolio</h1>
-      <h2 className="text-3xl text-center items-center justify-center">Welcome to my portfolio</h2>
-      <h3 className="text-2xl text-center items-center justify-center">Here are some of my projects</h3>
+      <h1 className="text-5xl text-center items-center justify-center">
+        My Portfolio
+      </h1>
+      <h2 className="text-3xl text-center items-center justify-center">
+        Welcome to my portfolio
+      </h2>
+      <h3 className="text-2xl text-center items-center justify-center">
+        Here are some of my projects
+      </h3>
       <ul className="text-2xl text-center items-center justify-center">
-        <li className="text-2xl text-center items-center justify-center">Project 1</li>
-        <li className="text-2xl text-center items-center justify-center">Project 2</li>
-        <li className="text-2xl text-center items-center justify-center">Project 3</li>
-        <li className="text-2xl text-center items-center justify-center">Project 4</li>
-        <li className="text-2xl text-center items-center justify-center">Project 5</li>
-        <li className="text-2xl text-center items-center justify-center">Project 6</li>
-        <li className="text-2xl text-center items-center justify-center">Project 7</li>
-        <li className="text-2xl text-center items-center justify-center">Project 8</li>
-        <li className="text-2xl text-center items-center justify-center">Project 9</li>
-        <li className="text-2xl text-center items-center justify-center">Project 10</li>
-        <li className="text-2xl text-center items-center justify-center">Project 11</li>
-        <li className="text-2xl text-center items-center justify-center">Project 12</li>
-        <li className="text-2xl text-center items-center justify-center">Project 13</li>
-        </ul>
-     
-
-
+        <li className="text-2xl text-center items-center justify-center">
+          Project 1
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 2
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 3
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 4
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 5
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 6
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 7
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 8
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 9
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 10
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 11
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 12
+        </li>
+        <li className="text-2xl text-center items-center justify-center">
+          Project 13
+        </li>
+      </ul>
     </div>
   );
 };
