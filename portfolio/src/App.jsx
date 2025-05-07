1edc3cc428aef9b761e796d6f861610d50e70e2e
MogesBekele <mogesbekele32@gmail.com>
2025-05-07 16:43:18 +0300

    tailwind appied

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,13 +3,10 @@ import React from "react";
 const App = () => {
   return (
     <div className="text-5xl text-center items-center justify-center ">
-      <h1 className="text-5xl text-center items-center justify-center ">
-        Hello World!
-      </h1>
-      
+     
+     
     </div>
   );
-  
 };
 
 export default App;
